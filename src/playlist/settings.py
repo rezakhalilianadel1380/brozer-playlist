b05cc7533d910928174252420e26173fbac3edf4
Reza Khalilian <reza.kh.adel@gmail.com>
2024-01-21 16:57:24 +0330

    DIVID INSTALL APP TO THREE CATEGORY

--- src/playlist/settings.py
+++ src/playlist/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+OTHER_APPS = []
+LOCAL_APPS = []
+
+
+INSTALLED_APPS = DJANGO_APPS+OTHER_APPS+LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
