12c1aa66e5c139058c8a93966b93ba2f7811f9b3
Reza Khalilian <reza.kh.adel@gmail.com>
2024-01-21 16:54:47 +0330

    add .env to proejct

--- src/playlist/settings.py
+++ src/playlist/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r+#fne+4ow$27w)l6d=*y_%#&(9s^o#jibh)w5@1&&=gikhjy='
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
